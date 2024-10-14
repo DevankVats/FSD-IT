3be8bb3df07d08966f774c6cdb0c223eeb427fde
DevankVats <vatsdevank2610@gmail .com>
2024-10-14 13:09:56 +0530

    Day 6

---
+++ Day6/script.js
@@ -0,0 +1,41 @@
+// let a=()=>{
+//     console.log("Arrow Function");
+// }  
+
+// let b=()=>{
+//     console.log("second arrow function");
+// }
+
+// a();
+// b();
+
+// let c=(x,y)=>{
+//     return x+y;
+// }
+// let res=c("Rahul","Ravi");
+// let res1=c(12,22);
+// console.log(res);
+// console.log(res1);
+
+// function display(){
+//     // let p1=document.querySelector("p");
+//     let p1=document.querySelectorAll("p");
+//     p1[0].innerHTML="I am in para";
+//     p1[0].style.backgroundColor="red";
+//     p1[1].innerHTML="I am in para 2";
+//     p1[1].style.backgroundColor="green";
+// }
+
+let chanageHeader=()=>{
+    let h1=document.querySelector("h1");
+    h1.textContent="hann Ji Good Morning";
+    
+}
+
+let chanageHeader1=()=>{
+    let a1=document.querySelector("a");
+    a1.setAttribute("href","https://aktu.ac.in");
+    a1.innerHTML="AKTU Website"
+}
+
+
